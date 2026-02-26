abde912a6274b1d3b108c9068bd4d0ed8e7ea19e
NgoGiaBao2203 <giabaongo75@gmail.com>
2026-02-26 18:05:12 +0700

    completed the tiktok project

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const gifts = ["CPU i9", "RAM 32GB", "SSD 1TB"];
 
 function App() {
+  const [gift, setGift] = useState();
+
+  const ramdomGift = () => {
+    const index = Math.floor(Math.random() * gifts.length);
+    // Cập nhật State bằng phần quà tại vị trí index vừa lấy được
+    setGift(gifts[index]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 30 }}>
+      <h1>{gift || "Chua co phan thuong"}</h1>
+      <button onClick={ramdomGift}> Lay thuong </button>
     </div>
   );
 }
-
 export default App;
